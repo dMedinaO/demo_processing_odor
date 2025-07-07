34794c8a4bb8ef54e66529109abe6e1c2c033687
Avellaneda Tamayo <javellan@compchem16.ipb-halle.de>
2025-07-07 17:32:17 +0200

    update gitignore

--- src/common_funtions.py
+++ src/common_funtions.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import (silhouette_score, davies_bouldin_score, calinski_harabasz_score)
 import numpy as np
+import pandas as pd
 
 def estimated_metrics(dataset, labels, name_method):
 
@@ -23,3 +24,29 @@ def apply_clustering(clustering_method, dataset, name_method, **kwargs):
     metrics = estimated_metrics(dataset, model.labels_, name_method)
 
     return model, metrics
+
+def get_proportion_labels(labels):
+    unique, counts = np.unique(labels, return_counts=True)
+    proportions = dict(zip(unique, counts / len(labels)))
+    return proportions
+    
+def run_exploration(dataset, dict_params, clustering_method, name_method):
+    
+    matrix_result = []
+    
+    for element in dict_params:
+        id_test = element.get("id_test", None)
+        model, metrics = apply_clustering(clustering_method,
+                                      dataset,
+                                      name_method
+                                      **element.get("params", {}))
+        row = {"id_test": id_test, "name_method": name_method, 
+               "silhouette_score": metrics["silhouette_score"],
+               "n_labels": len(np.unique(model.labels_)),
+               "proportion_labels": get_proportion_labels(model.labels_),
+               "davies_bouldin_score": metrics["davies_bouldin_score"],
+               "calinski_harabasz_score": metrics["calinski_harabasz_score"]}
+        matrix_result.append(row)
+    
+    df_result = pd.DataFrame(matrix_result)
+    return df_result
